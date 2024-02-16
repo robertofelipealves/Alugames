f202b4cfcab6007abf73454b1b3698e4c041aae8
robertofelipealves <156329945+robertofelipealves@users.noreply.github.com>
2024-02-16 14:04:36 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+   
+    if (imagem.classList.contains ('dashboard__item__img--rented')) {
+        imagem.classList.remove ('dashboard__item__img--rented');
+        botao.textContent = 'Alugar';
+        botao.classList.remove ('dashboard__item__button--return');
+        alert ('Devolvido');
+    } else {
+        imagem.classList.add ('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add ('dashboard__item__button--return');
+        
+    }
+}    
